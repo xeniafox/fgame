699da11434b42e04d5b9bfaba2c5c397543ef217
Lavínia Rodriguês <xenia@kernel32.xyz>
2024-03-28 03:24:26 -0300

    feat: add CPU-based vector type aliases

---
+++ include/fgame/math.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <mathfu/vector.h>
+
+namespace fgame {
+namespace math {
+
+using cpu_vec2 = mathfu::Vector<float, 2>;
+using cpu_vec3 = mathfu::Vector<float, 3>;
+
+} // namespace math
+} // namespace fgame
