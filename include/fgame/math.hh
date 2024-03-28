18336e7babd7df05fffd24d04eba04684245df00
Lavínia Rodriguês <xenia@kernel32.xyz>
2024-03-28 03:30:37 -0300

    feat: add a generalized CPU-based vector type alias

--- include/fgame/math.hh
+++ include/fgame/math.hh
@@ -5,6 +5,8 @@
 namespace fgame {
 namespace math {
 
+template <class T, std::size_t n> using cpu_vec = mathfu::Vector<T, n>;
+
 using cpu_vec2 = mathfu::Vector<float, 2>;
 using cpu_vec3 = mathfu::Vector<float, 3>;
 
